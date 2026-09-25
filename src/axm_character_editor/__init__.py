35bd9ffef437beb60d9d46da0c6ad825e763ff3e
mike-axiom-mir <miketobi90@gmail.com>
2026-09-25 22:08:59 +0200

    feat: expose human-v0 face and GLB build APIs

--- src/axm_character_editor/__init__.py
+++ src/axm_character_editor/__init__.py
@@ -9,6 +9,15 @@ from .blueprint import (
     signature_bundle,
     validate_blueprint,
 )
+from .human_face import HumanFaceError, face_summary, write_obj as write_face_obj
+from .human_asset import (
+    HumanAssetError,
+    build_glb,
+    build_package,
+    verify_glb,
+    verify_glb_path,
+    write_glb,
+)
 
 __all__ = [
     "BLUEPRINT_SCHEMA",
@@ -18,4 +27,13 @@ __all__ = [
     "new_blueprint",
     "signature_bundle",
     "validate_blueprint",
+    "HumanFaceError",
+    "face_summary",
+    "write_face_obj",
+    "HumanAssetError",
+    "build_glb",
+    "build_package",
+    "verify_glb",
+    "verify_glb_path",
+    "write_glb",
 ]
